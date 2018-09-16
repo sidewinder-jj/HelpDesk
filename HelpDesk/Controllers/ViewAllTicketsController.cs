29e6054512e2fd18e90fa7f6c7a1073a7fae6e58
Jamie Jordan <jamie_a_jordan@hotmail.co.uk>
2018-09-16 21:15:28 +0100

    View ticket page and links to view ticket from view all tickets page

---
+++ HelpDesk/Controllers/ViewAllTicketsController.cs
@@ -0,0 +1,34 @@
+﻿using HelpDesk.Models;
+using HelpDesk.ViewModel;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HelpDesk.Controllers
+{
+    public class ViewAllTicketsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ViewAllTicketsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var tickets = _context.Tickets.ToList();
+
+            foreach (var ticket in tickets)
+            {
+                TicketRowViewModel viewModel = new TicketRowViewModel();
+
+                viewModel.Summary = ticket.Summary;
+                viewModel.DateSubmitted = DateTimeOffset.UtcNow;
+            }
+
+            return View(tickets);
+        }
+    }
+}
---
+++ HelpDesk/ViewModel/TicketRowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.ViewModel
+{
+    public class TicketRowViewModel
+    {
+        [Required]
+        public string Summary { get; set; }
+
+        [Required]
+        public DateTimeOffset DateSubmitted { get; set; }
+    }
+}
