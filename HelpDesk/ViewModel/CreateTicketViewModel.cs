f0c860dc3f163283b17c10ffa347a2be3c0553fc
Jamie Jordan <jamie_a_jordan@hotmail.co.uk>
2018-09-17 00:09:36 +0100

    Adds untracked file

---
+++ HelpDesk/ViewModel/CreateTicketViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.ViewModel
+{
+    public class CreateTicketViewModel
+    {
+        [Required]
+        public string Summary { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
