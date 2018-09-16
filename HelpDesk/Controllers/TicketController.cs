8717fbc117a15f65d06ddc19d17cb92dbd1fff3f
Jamie Jordan <jamie_a_jordan@hotmail.co.uk>
2018-09-16 21:10:43 +0100

    View ticket page and links to view ticket from view all tickets page

--- HelpDesk/Controllers/TicketController.cs
+++ HelpDesk/Controllers/TicketController.cs
@@ -37,5 +37,19 @@ namespace HelpDesk.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        public ActionResult View(int ticketId)
+        {
+            var ticket = _context.Tickets.Find(ticketId);
+            var viewModel = new Ticket
+            {
+                Description = ticket.Description,
+                Id = ticket.Id,
+                Summary = ticket.Summary
+            };
+
+            return View(ticket);
+        }
     }
 }
